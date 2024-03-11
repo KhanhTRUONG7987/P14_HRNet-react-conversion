6a67813edc13295577c0060cf5650c89e092cba3
khanh.truong <abby79truong@gmail.com>
2024-03-11 23:47:11 +0100

    update style for form

--- hrnet-react-conversion-app/src/components/CreateEmployeeForm.js
+++ hrnet-react-conversion-app/src/components/CreateEmployeeForm.js
@@ -72,93 +72,112 @@ const CreateEmployeeForm = ({ saveEmployee }) => {
         <div className="container" onClick={stopPropagation}>
             <h2>Create Employee</h2>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="first-name">First Name</label>
-                <input
-                    type="text"
-                    id="first-name"
-                    value={employeeData.firstName}
-                    onChange={handleChange}
-                    name="firstName"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="first-name">First Name</label>
+                    <input
+                        type="text"
+                        id="first-name"
+                        value={employeeData.firstName}
+                        onChange={handleChange}
+                        name="firstName"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="last-name">Last Name</label>
-                <input
-                    type="text"
-                    id="last-name"
-                    value={employeeData.lastName}
-                    onChange={handleChange}
-                    name="lastName"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="last-name">Last Name</label>
+                    <input
+                        type="text"
+                        id="last-name"
+                        value={employeeData.lastName}
+                        onChange={handleChange}
+                        name="lastName"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="birthday">Date of Birth</label>
-                <DatePicker
-                    selected={employeeData.birthday}
-                    onChange={handleBirthdayChange}
-                    className="date-picker"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="birthday">Date of Birth</label>
+                    <DatePicker
+                        selected={employeeData.birthday}
+                        onChange={handleBirthdayChange}
+                        className="date-picker"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="start-date">Start Date</label>
-                <DatePicker
-                    selected={employeeData.startDate}
-                    onChange={handleStartDateChange}
-                    className="date-picker"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="start-date">Start Date</label>
+                    <DatePicker
+                        selected={employeeData.startDate}
+                        onChange={handleStartDateChange}
+                        className="date-picker"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="street">Street</label>
-                <input
-                    type="text"
-                    id="street"
-                    value={employeeData.address.street}
-                    onChange={handleChange}
-                    name="address.street"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="street">Street</label>
+                    <input
+                        type="text"
+                        id="street"
+                        value={employeeData.address.street}
+                        onChange={handleChange}
+                        name="address.street"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="city">City</label>
-                <input
-                    type="text"
-                    id="city"
-                    value={employeeData.address.city}
-                    onChange={handleChange}
-                    name="address.city"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="city">City</label>
+                    <input
+                        type="text"
+                        id="city"
+                        value={employeeData.address.city}
+                        onChange={handleChange}
+                        name="address.city"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="state">State</label>
-                <input
-                    type="text"
-                    id="state"
-                    value={employeeData.address.state}
-                    onChange={handleChange}
-                    name="address.state"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="state">State</label>
+                    <input
+                        type="text"
+                        id="state"
+                        value={employeeData.address.state}
+                        onChange={handleChange}
+                        name="address.state"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="zip">Zip Code</label>
-                <input
-                    type="text"
-                    id="zip"
-                    value={employeeData.address.zip}
-                    onChange={handleChange}
-                    name="address.zip"
-                    required={true}
-                />
+                <div className="form-row">
+                    <label htmlFor="zip">Zip Code</label>
+                    <input
+                        type="text"
+                        id="zip"
+                        value={employeeData.address.zip}
+                        onChange={handleChange}
+                        name="address.zip"
+                        required={true}
+                    />
+                </div>
 
-                <label htmlFor="department">Department</label>
-                <Dropdown
-                    className='department'
-                    options={['HR', 'Finance', 'Marketing']}
-                    value={employeeData.department}
-                    onChange={(option) => setEmployeeData(prevState => ({
-                        ...prevState,
-                        department: option.value
-                    }))}
-                    placeholder="Select an option"
-                />
+                <div className="form-row">
+                    <label htmlFor="department">Department</label>
+                    <Dropdown
+                        className='department'
+                        options={['HR', 'Finance', 'Marketing']}
+                        value={employeeData.department}
+                        onChange={(option) => setEmployeeData(prevState => ({
+                            ...prevState,
+                            department: option.value
+                        }))}
+                        placeholder="Select an option"
+                    />
+                </div>
+
                 <button type="submit">Save</button>
             </form>
         </div>
